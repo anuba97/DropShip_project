d920aa30088a5664fea9f71e84dd0ec8b3faa6c0
eddy961206 <89622709+eddy961206@users.noreply.github.com>
2023-03-24 18:23:13 +0900

    Add files via upload

--- dbdb/dropuser작품,아티스트,관리자 테이블 및 시퀀스 생성.sql
+++ dbdb/dropuser작품,아티스트,관리자 테이블 및 시퀀스 생성.sql
@@ -18,42 +18,46 @@ CREATE TABLE Work
     work_sale            NUMBER(4,2)       DEFAULT 0.00 NOT NULL, -- 세일 퍼센트
     work_isBest          NUMBER(1)         DEFAULT 0 NOT NULL, 
     work_reg_date        DATE              DEFAULT sysdate NOT NULL, 
-    work_price           NUMBER(7)         DEFAULT 10000 NOT NULL, 
+    work_price           NUMBER(9)         DEFAULT 10000 NOT NULL, 
     work_dp              VARCHAR2(500)     DEFAULT 'https://www.google.com/maps?cid=13363865620386383060' NULL, 
     work_hit             NUMBER(4)         DEFAULT 0 NOT NULL, 
     admin_id             NUMBER(3)         DEFAULT 1 NULL, 
+    work_available       NUMBER(1)         DEFAULT 1 NULL, 
      PRIMARY KEY (id)
 );
+
+
 
 -- Auto Increment를 위한 Sequence 추가 SQL - Work.id
 CREATE SEQUENCE Work_SEQ
 START WITH 1
 INCREMENT BY 1;
 
-DROP SEQUENCE Work_SEQ; 
+--DROP SEQUENCE Work_SEQ; 
 
 --------------- Artist작가 테이블, artist_seq 생성
 CREATE TABLE Artist
 (
     id                     NUMBER(4)         NOT NULL, 
-    artist_korean_name     VARCHAR2(300)     NOT NULL, 
-    artist_english_name    VARCHAR2(300)     NOT NULL, 
-    artist_img_url         VARCHAR2(500)     NOT NULL, 
-    artist_country         VARCHAR2(100)     NOT NULL, 
-    artist_birth_death     VARCHAR2(100)     NOT NULL, 
-    artist_main            VARCHAR2(200)     NOT NULL, 
-    artist_content         VARCHAR2(4000)    NOT NULL, 
+    artist_korean_name     VARCHAR2(300)      NULL, 
+    artist_english_name    VARCHAR2(300)      NULL, 
+    artist_img_url         VARCHAR2(500)      NULL, 
+    artist_country         VARCHAR2(100)      NULL, 
+    artist_birth_death     VARCHAR2(100)      NULL, 
+    artist_main            VARCHAR2(200)      NULL, 
+    artist_content         VARCHAR2(4000)     NULL, 
      PRIMARY KEY (id)
 );
-desc artist;
-alter table artist add artist_img_url VARCHAR2(500) NOT NULL;
+
+--desc artist;
+--alter table artist add artist_img_url VARCHAR2(500) NOT NULL;
 
 -- Auto Increment를 위한 Sequence 추가 SQL - Artist.id
 CREATE SEQUENCE Artist_SEQ
 START WITH 1
 INCREMENT BY 1;
 
-DROP SEQUENCE Artist_SEQ;
+--DROP SEQUENCE Artist_SEQ;
 
 -- drop table artist CASCADE CONSTRAINTS;
 --alter table artist modify artist_content varchar2(4000);
@@ -62,23 +66,23 @@ CREATE TABLE Admin
 (
     id                NUMBER(3)       NOT NULL, 
     admin_name        VARCHAR2(30)    NOT NULL, 
-    admin_id          VARCHAR2(20)    NOT NULL, 
+    admin_login_id    VARCHAR2(20)    NOT NULL, 
     admin_pw          VARCHAR2(40)    NOT NULL, 
     admin_isSuper     NUMBER(1)       DEFAULT 0 NOT NULL, 
-    admin_phone       NUMBER(10)      NOT NULL, 
+    admin_phone       VARCHAR2(12)    NOT NULL, 
     admin_reg_date    DATE            DEFAULT sysdate NOT NULL, 
     admin_ban         NUMBER(1)       DEFAULT 0 NOT NULL, 
      PRIMARY KEY (id)
 );
 
---alter table admin modify admin_phone NUMBER(10);
+--alter table admin modify admin_phone VARCHAR2(12);
 
 -- Auto Increment를 위한 Sequence 추가 SQL - Admin.id
 CREATE SEQUENCE Admin_SEQ
 START WITH 1
 INCREMENT BY 1;
 
-DROP SEQUENCE Admin_SEQ; 
+--DROP SEQUENCE Admin_SEQ; 
 
 ------- 외래키 한꺼번에 설정
 -- Work(admin_id) -> Admin(id)
